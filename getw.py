d7590f026c34f9fa56953d62cd2101bc4f51e836
gelemani <88558485+gelemani@users.noreply.github.com>
2023-10-07 22:56:55 +0300

    Update getw.py

--- getw.py
+++ getw.py
@@ -1,6 +1,8 @@
 import telebot, requests as r
-from config import tele_token, API
-
+# from config import tele_token, API
+weather_token = '2b20bbfe528a233676b17af140a02cb1'
+tele_token = '5809893785:AAH5nFUVyyouQpfXoWC1hTOLIPKOdqEiQqQ'
+API = '0cc65a68bce33a4a462fe88885e49314'
 
 bot = telebot.TeleBot(tele_token)
 
